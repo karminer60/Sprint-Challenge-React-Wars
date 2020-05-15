1aaf26e17d4082f1a848290949718dfbb67ac2d1
karina-rodriguez <karminer60@gmail.com>
2020-05-15 09:47:16 -0700

    starting character component

---
+++ starwars/src/Character.js
@@ -0,0 +1,10 @@
+const Character = () => {
+    
+    return (
+      <div className="Characters">
+        <h1 className="Character">Character</h1>
+      </div>
+    );
+  }
+  
+  export default Character;
