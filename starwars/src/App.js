fbdce782014d9d016c4c53142674136cc5a0e88f
karina-rodriguez <karminer60@gmail.com>
2020-05-15 09:41:35 -0700

    fetched data from star wars API

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,6 +10,20 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState(); 
+  useEffect(() => {
+  
+    axios.get(`https://swapi.dev/api/people`)
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
+  console.log(data);
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
